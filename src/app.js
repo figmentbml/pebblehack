c9eaa02e816f2b2ca100ee387fb7f167d85d9fc1
Beth <Beth.jaswa@gmail.com>
2015-09-05 12:31:58 -0600

    list view to detail view works

--- src/app.js
+++ src/app.js
@@ -1,6 +1,41 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
 
+var companions = [
+  {
+    id: 1,
+    name: 'Urban',
+    icon: 'images/menu_icon.png',
+    location: 'location',
+    date: 'date acquired',
+    description: 'lots of text'   
+  },
+  {
+    id: 2,
+    name: 'Trees',
+    icon: 'images/menu_icon.png',
+    location: 'location',
+    date: 'date acquired',
+    description: 'lots of text'   
+  },
+  {
+    id: 3,
+    name: 'Water',
+    icon: 'images/menu_icon.png',
+    location: 'location',
+    date: 'date acquired',
+    description: 'lots of text'   
+  },
+  {
+    id: 4,
+    name: 'Other',
+    icon: 'images/menu_icon.png',
+    location: 'location',
+    date: 'date acquired',
+    description: 'lots of text'   
+  }
+];
+
 var main = new UI.Card({
   title: 'GeoMonster',
   icon: 'images/menu_icon.png',
@@ -30,31 +65,32 @@ main.on('click', 'up', function(e) {
 });
 
 main.on('click', 'select', function(e) {
-  var menu = new UI.Menu({
+  var items = [];
+  for (var i=0; i<companions.length; i++) {
+    items.push({
+      title: companions[i].name,
+      subtitle: companions[i].location + ' ' + companions[i].date,
+      icon: companions[i].icon
+    });
+  }
+  var companionList = new UI.Menu({
     fullscreen: true,
     sections: [{
-      title: 'Acquired Monsters',
-      items: [{
-        title: 'Monster name',
-        subtitle: 'location',
-        body: 'date added',
-        icon: 'images/menu_icon.png'
-      }, {
-        title: 'Monster name',
-        subtitle: 'location',
-        body: 'date added',
-        icon: 'images/menu_icon.png'
-      }]
+      title: 'Acquired Companions',
+      items: items
     }]
   });
-  menu.add(sections);
-  menu.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
+  companionList.show();
+  companionList.on('select', function(e) {
+    console.log('menu item was clicked!');
+    console.log(e.itemIndex);
+    var detailcard = new UI.Card({
+      title: companions[e.itemIndex].name,
+      subtitle: companions[e.itemIndex].location + ' ' + companions[e.itemIndex].date,
+      icon: companions[e.itemIndex].icon,
+      body: companions[e.itemIndex].description,
+      scrollable: true
+    });
+    detailcard.show();
+  });
 });
---
+++ src/companions.js
@@ -0,0 +1,36 @@
+// don't use this
+
+var companions = [
+  {
+    id: 1,
+    name: 'Urban',
+    icon: 'images/menu_icon.png',
+    location: 'location',
+    date: 'date acquired',
+    description: 'lots of text'   
+  },
+  {
+    id: 2,
+    name: 'Trees',
+    icon: 'images/menu_icon.png',
+    location: 'location',
+    date: 'date acquired',
+    description: 'lots of text'   
+  },
+  {
+    id: 3,
+    name: 'Water',
+    icon: 'images/menu_icon.png',
+    location: 'location',
+    date: 'date acquired',
+    description: 'lots of text'   
+  },
+  {
+    id: 4,
+    name: 'Other',
+    icon: 'images/menu_icon.png',
+    location: 'location',
+    date: 'date acquired',
+    description: 'lots of text'   
+  }
+];
